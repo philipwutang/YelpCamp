5db1ca922318f663ed9d6913a508025651916612
Philip Tang <philip.tang@uwaterloo.ca>
2017-07-17 23:08:51 -0400

    upload

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const indexRoutes = require("./routes/index");
 
 const app = express();
 
-const databaseUrl = process.env.DATABASEURL || "mongodb://localhost/yelp_camp";
+const databaseUrl = "mongodb://PhilipWuTang:PhiladelphiaT0321@ds133279.mlab.com:33279/philiptang-yelpcamp";
 mongoose.connect(databaseUrl);
 
 app.use(bodyParser.urlencoded({ extended: true }));
